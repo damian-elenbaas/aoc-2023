b00b017bc080d9442c98269a8db3e6a2a9628a98
Damian Elenbaas <damianelenbaas2003@gmail.com>
2023-12-04 13:45:25 +0100

    Day 4.2

--- day-04/src/main.ts
+++ day-04/src/main.ts
@@ -3,11 +3,13 @@ import * as fs from 'fs';
 interface Card {
   winningNumbers: number[];
   givenNumbers: number[];
+  copies: number;
 }
 
 function main() {
   const input = fs.readFileSync('./input.txt', 'utf8');
   part1(input);
+  part2(input);
 }
 
 function part1(input: string) {
@@ -16,7 +18,6 @@ function part1(input: string) {
     if(card === '') return;
 
     const cardSplit = card.split(':');
-    const game = cardSplit[0];
     const cardNumbers = cardSplit[1].split('|');
     const winningNumbers = cardNumbers[0]
       .trim()
@@ -39,7 +40,55 @@ function part1(input: string) {
     sum += score;
   });
 
-  console.log(sum);
+  console.log(`Part 1: ${sum}`);
+}
+
+function part2(input: string) {
+  let cards: Card[] = [];
+  input.split('\r\n').forEach((card) => {
+    if(card === '') return;
+
+    const cardSplit = card.split(':');
+    const cardNumbers = cardSplit[1].split('|');
+    const winningNumbers = cardNumbers[0]
+      .trim()
+      .split(' ')
+      .map((n) => parseInt(n))
+      .filter((n) => !isNaN(n));
+    const givenNumbers = cardNumbers[1]
+      .trim()
+      .split(' ')
+      .map((n) => parseInt(n))
+      .filter((n) => !isNaN(n));
+
+    cards.push({
+      winningNumbers,
+      givenNumbers,
+      copies: 1
+    });
+  });
+
+  cards.forEach((card, i) => {
+    let count = 0;
+
+    card.givenNumbers.forEach((n) => {
+      if(card.winningNumbers.includes(n)) count++;
+    });
+
+    for(let x = 1; x <= count; x++) {
+      let nextCard = cards[i + x];
+      if(!nextCard) break;
+
+      nextCard.copies += card.copies;
+    }
+  });
+
+  let totalCopies = 0;
+  cards.forEach((card) => {
+    totalCopies += card.copies;
+  });
+
+  console.log(`Part 1: ${totalCopies}`);
 }
 
 main();
